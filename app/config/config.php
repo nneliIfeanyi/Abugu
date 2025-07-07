84abf72ae12c900b6708052d7eb975413e7fec45
Stanvic <stanvicbest@gmail.com>
2025-07-07 17:11:40 +0100

    config&htaccessCorrected

--- app/config/config.php
+++ app/config/config.php
@@ -1,27 +1,27 @@
 <?php
 // DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "abugu");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "abugu");
 
-// App Root
-define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
-define('URLROOT', 'http://localhost/st-mary');
-// Site Name
-define('SITENAME', 'ST. MARY\'S POLY');
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'http://localhost/st-mary');
+// // Site Name
+// define('SITENAME', 'ST. MARY\'S POLY');
 
 
 // DB Params
-//define("DB_HOST", "localhost");
-//define("DB_USER", "root");
-//define("DB_PASS", "");
-//define("DB_NAME", "Nice-school");
+define("DB_HOST", "localhost");
+define("DB_USER", "obrownho_alex");
+define("DB_PASS", "Alexander@2025");
+define("DB_NAME", "obrownho_smp");
 
-// App Root
-//define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
-//define('URLROOT', 'https://sample.stanvic.com.ng');
-// Site Name
-//define('SITENAME', 'NiceSchool');
+//App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+//URL Root
+define('URLROOT', 'https://stmarypolytechnic.com.ng');
+//Site Name
+define('SITENAME', 'ST. MARY\'S POLY');
